737342d82164df7a0f4c1231b2204358def19c81
Madaga95 <74613897+Madaga95@users.noreply.github.com>
2022-06-17 23:05:01 +0200

    Ajoutez des fichiers projet.

---
+++ GénérateurMotDePasse/Program.cs
@@ -0,0 +1,79 @@
+﻿using FormationCS;
+using System;
+
+namespace GénérateurMotDePasse
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            const int NB_MOTS_DE_PASSE = 10;
+
+            int longueurMotDePasse = outils.DemanderNombrePositionNonNul("Longueur du mot de passe : ");
+
+            Console.WriteLine();
+
+            int choixAlphabet = outils.DemanderNombreEntre("Vous voulez un mot de passe avec :\n" +
+                "1 - Uniquement des caractères en minuscule\n" +
+                "2 - Des caractère minuscules et majuscules\n" +
+                "3 - Des caractères et des chiffres\n" +
+                "4 - Caratères, chiffres et caratères spéciaux\n" +
+                "Votre choix : ", 1, 4);
+
+
+
+            string minuscules = "abcdefghijklmnopqrstuvwxyz";
+            int chiffres = 0123456789;
+            string caractereSpeciaux = "&~#@$";
+            string majuscule = minuscules.ToUpper();
+            string alphabet;
+            
+            string motDePasse = "";
+            
+
+
+            Random rand = new Random();
+
+
+            // boucle sur le choix du mot de passe
+
+            if(choixAlphabet == 1)
+            {
+                alphabet = minuscules;
+            }
+            else if(choixAlphabet == 2)
+            {
+                alphabet = minuscules + majuscule;
+            }
+            else if (choixAlphabet == 3)
+            {
+                alphabet = minuscules + majuscule + chiffres;
+            }
+            else
+            {
+                alphabet = minuscules + majuscule + chiffres + caractereSpeciaux;
+            }
+
+            int longueurAlphabet = alphabet.Length;
+
+
+
+            // boucle sur la longueyr du mot de passe
+            for (int j = 0; j < NB_MOTS_DE_PASSE; j++)
+            {
+                motDePasse = "";
+                for (int i = 0; i < longueurMotDePasse; i++)
+                {
+                    int index = rand.Next(0, longueurAlphabet);
+                    motDePasse += alphabet[index];
+
+                }
+                Console.WriteLine("Mot de passe : " + motDePasse);
+            }
+            
+            
+
+        }
+    }
+}
---
+++ GénérateurMotDePasse/outils.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormationCS
+{
+    static class outils
+    {
+
+         public static int DemanderNombrePositionNonNul(string question)
+        {
+            return DemanderNombreEntre(question, 1, int.MaxValue, "ERREUR : le nombre doit être positif et non nul ");
+        }
+        public static int DemanderNombreEntre(string question, int min, int max, string messageErreurPersonnaliser = null)
+        {
+            while (true)
+            {
+                int nombre = DemanderNombre(question);
+
+                if ((nombre >= min) && (nombre <= max))
+                {
+                    
+                   // valide
+                   
+                    
+                   return nombre;
+                }
+                if (messageErreurPersonnaliser == null)
+                {
+                    Console.WriteLine("Le nombre doit être compris entre " + min + " et " + max);
+                }
+                else
+                {
+                    Console.WriteLine(messageErreurPersonnaliser);
+                }
+                
+                Console.WriteLine();
+                return DemanderNombreEntre(question, min, max);
+            }
+        }
+
+        public static int DemanderNombre(string question)
+        {
+            while (true)                                     // boucler tant qu'on a pas reçu une réponse valide (qui contient que des chiffres)
+            {
+                Console.WriteLine(question);               //poser la question
+                string reponse = Console.ReadLine();      //récupérer la réponse
+                try                                      // gerer l'erreur de conversion
+                {
+                    int reponseInt = int.Parse(reponse); // convertir, gerer l'erreur de conversion
+                    return reponseInt;
+                }
+                catch
+                {
+                    Console.WriteLine("ERREUR : Vous devez rentrer un nombre");
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
